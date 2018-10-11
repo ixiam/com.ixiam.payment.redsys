c092dab18612a99221ff4712112619df057cf0e4
Ruben <r.pineda@ixiam.com>
2018-10-11 16:15:40 +0200

    Adapt to the new version CiviCRM 5 and better coding standars

--- CRM/Redsys/Form/Settings.php
+++ CRM/Redsys/Form/Settings.php
@@ -1,6 +1,7 @@
 <?php
 
 require_once 'CRM/Core/Form.php';
+use CRM_Redsys_ExtensionUtil as E;
 
 class CRM_Redsys_Form_Settings extends CRM_Core_Form {
   public function buildQuickForm() {
@@ -8,17 +9,17 @@ class CRM_Redsys_Form_Settings extends CRM_Core_Form {
     $this->add('text', 'merchant_terminal', 'Merchant Terminal', array('size' => 5));
 
     $paymentProcessors = $this->getPaymentProcessors();
-    foreach( $paymentProcessors as $paymentProcessor ) {
-      $settingCode = 'merchant_terminal_' . $paymentProcessor[ "id" ];
-      $settingTitle = $paymentProcessor[ "name" ] . " (" .
-        ( $paymentProcessor["is_test"] == 0 ? "Live" : "Test" ) . ")";
+    foreach ($paymentProcessors as $paymentProcessor) {
+      $settingCode = 'merchant_terminal_' . $paymentProcessor["id"];
+      $settingTitle = $paymentProcessor["name"] . " (" .
+        ($paymentProcessor["is_test"] == 0 ? "Live" : "Test") . ")";
       $this->add('text', $settingCode, $settingTitle, array('size' => 5));
     }
 
     $this->addButtons(array(
       array(
         'type' => 'submit',
-        'name' => ts('Submit'),
+        'name' => E::ts('Submit'),
         'isDefault' => TRUE,
       ),
     ));
@@ -38,30 +39,32 @@ class CRM_Redsys_Form_Settings extends CRM_Core_Form {
     $values = $this->exportValues();
     $redsys_settings['ipn_http'] = $values['ipn_http'];
     $redsys_settings['merchant_terminal'] = $values['merchant_terminal'];
-    
+
     $paymentProcessors = $this->getPaymentProcessors();
-    foreach( $paymentProcessors as $paymentProcessor ) {
-      $settingId = 'merchant_terminal_' . $paymentProcessor[ "id" ];
+    foreach ($paymentProcessors as $paymentProcessor) {
+      $settingId = 'merchant_terminal_' . $paymentProcessor["id"];
       $redsys_settings[$settingId] = $values[$settingId];
     }
-    
+
     CRM_Core_BAO_Setting::setItem($redsys_settings, "Redsys Settings", 'redsys_settings');
-    CRM_Core_Session::setStatus(ts('Redsys Settings Saved', array( 'domain' => 'com.ixiam.payment.redsys')), 'Configuration Updated', 'success');
+    CRM_Core_Session::setStatus(E::ts('Redsys Settings Saved', array('domain' => 'com.ixiam.payment.redsys')), 'Configuration Updated', 'success');
 
     parent::postProcess();
   }
 
   public function getPaymentProcessors() {
-    // Get the Redsys payment processor type
-    $redsysName = array( 'name' => 'Redsys' );
-    $paymentProcessorType = civicrm_api3( 'PaymentProcessorType', 'getsingle', $redsysName );
+    // Get the Redsys payment processor type.
+    $redsysName = array('name' => 'Redsys');
+    $paymentProcessorType = civicrm_api3('PaymentProcessorType', 'getsingle', $redsysName);
 
-    // Get the payment processors of Redsys type
+    // Get the payment processors of Redsys type.
     $redsysType = array(
-      'payment_processor_type_id' => $paymentProcessorType[ 'id' ],
-      'is_active' => 1 );
-    $paymentProcessors = civicrm_api3( 'PaymentProcessor', 'get', $redsysType );
+      'payment_processor_type_id' => $paymentProcessorType['id'],
+      'is_active' => 1,
+    );
+    $paymentProcessors = civicrm_api3('PaymentProcessor', 'get', $redsysType);
 
     return $paymentProcessors["values"];
   }
+
 }
